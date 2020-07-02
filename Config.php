633f3bc568031afc38776385a5a4fd6a63dcfaaf
OtakuaErmo <luiz.paulus51@gmail.com>
2020-07-02 18:42:32 -0300

    criando a estrutura de pastas3

---
+++ Config.php
@@ -0,0 +1,11 @@
+<?php
+class Config
+{
+    const BD_TIPO = "mysql";
+    const BD_HOST = "localhost";
+    const BD_NOME = "db_tai_sisger";
+    const BD_PORTA = "3306";
+    const BD_USUARIO = "root";
+    const BD_SENHA = "";
+    const BD_CHARSET = "utf8mb4";
+}
